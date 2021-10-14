394907264502d662fe70af38a4b266feee65c727
Кирилл Квит <kbroom135@gmail.com>
2021-10-14 11:33:42 +0300

    1 task using only functional components

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,46 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Counter from '../views/Counter/index';
 
-class CounterContainer extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            value: 0
-        }
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-        this.reset = this.reset.bind(this);
-    }
+const CounterContainer = () =>{
+    const [value, setValue] = useState(0);
 
-    increment(){
-        const updated = this.state.value + 1;
-        this.setState({
-            value: updated
-        })
-    }
-
-    decrement(){
-        const updated = this.state.value - 1;
-        this.setState({
-            value: updated
-        })
-    }
-
-    reset(){
-        this.setState({
-            value: 0
-        })
-    }
-
-    render(){
-        const props = {
-            value: this.state.value,
-            increment: this.increment,
-            decrement: this.decrement,
-            reset: this.reset
-        };
-        return (<Counter {...props} />);
-    }
+    const props = {
+                    value: value,
+                    increment: () => { setValue(value + 1); },
+                    decrement: () => { setValue(value - 1); },
+                    reset: () => { setValue(0); }
+                };
+    return (<Counter {...props} />);
 }
 
 export default CounterContainer;
