774e41b69c7e29e80c1dc63408ac3ac537dcc67a
Кирилл Квит <kbroom135@gmail.com>
2021-10-19 18:22:41 +0300

    4 task

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-class AboutUs extends React.Component{
-    render(){
-        return(
-            <h1>About us page</h1>
-        );
-    }
+const AboutUs = () => {
+    return(
+        <h1>About us page</h1>
+    );
 }
 
 export default AboutUs;
